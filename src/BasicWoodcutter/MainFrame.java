043010a54e6a3694aea92ecfce8d3c8b71334027
Hejir Rashidzadeh <hejirrashidzadeh@Hejirs-MacBook-Pro.local>
2021-11-09 15:59:47 -0500

    Initial commit

---
+++ src/BasicWoodcutter/MainFrame.java
@@ -0,0 +1,61 @@
+package BasicWoodcutter;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.dreambot.api.methods.interactive.NPCs;
+import org.dreambot.api.wrappers.interactive.NPC;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
+import org.apache.commons.lang3.ArrayUtils;
+
+public class MainFrame extends JFrame{
+    private JButton OKButton;
+    private JButton clearButton;
+    private JLabel firstName;
+    private JTextField tflastName;
+    private JLabel lastName;
+    private JTextField tffirstName;
+    private JLabel Answer;
+    private JPanel mainPanel;
+
+    //this is a constructor that initializes parameters for an object of class MainFrame before it even exists
+    public MainFrame(){
+        setContentPane(mainPanel);
+        setTitle("Welcome");
+        setSize(450,300);
+        setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+        setVisible(true);
+
+        OKButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                java.util.List<NPC> localNPC = NPCs.all();
+                String[] names = new String[localNPC.size()];
+
+                for(int i=0; i < localNPC.size(); i++) {
+                    NPC element = localNPC.get(i);
+                    names[i] = element.getName();
+                }
+                //String firstName =tffirstName.getText();
+                //String lastName =tflastName.getText();
+                Answer.setText("Welcome " + Arrays.toString(names) );
+            }
+        });
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Answer.setText("");
+            }
+        });
+    }
+
+    public static void main(String[] args){
+        MainFrame myFrame = new MainFrame();
+    }
+
+    public static void main() {
+        MainFrame myFrame = new MainFrame();
+    }
+}
---
+++ src/BasicWoodcutter/TestScript.java
@@ -0,0 +1,113 @@
+package BasicWoodcutter;
+
+
+import org.dreambot.api.methods.container.impl.Inventory;
+import org.dreambot.api.methods.interactive.GameObjects;
+import org.dreambot.api.randoms.BankPinSolver;
+import org.dreambot.api.script.AbstractScript;
+import org.dreambot.api.script.Category;
+import org.dreambot.api.script.ScriptManifest;
+import org.dreambot.api.methods.MethodProvider;
+import org.dreambot.api.methods.Calculations;
+import org.dreambot.api.methods.filter.Filter;
+import org.dreambot.api.methods.map.Area;
+import org.dreambot.api.script.AbstractScript;
+import org.dreambot.api.script.Category;
+import org.dreambot.api.script.ScriptManifest;
+import org.dreambot.api.utilities.impl.Condition;
+import org.dreambot.api.wrappers.interactive.GameObject;
+import org.dreambot.api.wrappers.interactive.NPC;
+import org.dreambot.api.wrappers.interactive.Player;
+import org.dreambot.api.methods.container.impl.bank.Bank;
+
+
+import java.awt.*;
+import java.lang.reflect.Method;
+
+@ScriptManifest(
+        name = "Script Name",
+        description = "My script description!",
+        author = "Developer Name",
+        version = 1.0,
+        category = Category.WOODCUTTING,
+        image = "")
+
+
+public class TestScript extends AbstractScript {
+
+    @Override
+    public void onStart(){
+        //String[] strArray = new String[]{"Foo","Bar","Baz"};
+        //MainFrame.main(strArray);
+        MainFrame.main();
+        log("Hi2");
+    }
+
+    @Override
+    public int onLoop() {
+        /**GameObject tree = GameObjects.closest(gameObject -> gameObject !=null && gameObject.getName().equals("Tree"));
+        GameObject tree = GameObjects.closest(new Filter<GameObject>() {
+            @Override
+            public boolean match(GameObject gameObject) {
+                return gameObject !=null && gameObject.getName().equals("Tree");
+            }
+        });
+
+        MethodProvider.sleepUntil(() -> Inventory.count("Log") > countLog,600);
+        MethodProvider.sleepUntil(new Condition() {
+            @Override
+            public boolean verify() {
+                return false;
+            }
+        }; */
+
+
+        /** Creating an object called tree of class GameObject, we then call the method GameObjects.closest() which is a method with
+         *  return type static GameObject {aka an object for its return type}. This is then set equal to our tree which is of type
+         *  GameObject.
+         *  **/
+        GameObject tree = GameObjects.closest("Tree");
+        Player myPlayer = getLocalPlayer();
+
+        /** We then call the method interact() on our tree object which is a method of the class Interactable of which GameObject extends.
+         *  It returns a boolean to let us know if we were successful or not **/
+        if (!Inventory.isFull() && tree.interact("Chop down")){
+            int countLog = Inventory.count("Logs");
+            /** The first parameter of sleepWhile() is an interface, therefore you can use a lambda statement to pass the parameters to the
+             *  interface function, which in this case is verify() */
+            MethodProvider.sleepWhile(() -> tree.exists(),Calculations.random(8000,12000));
+
+
+        };
+
+        if (Inventory.isFull()){
+            if (Bank.openClosest()) {
+                // If this returns true, that means the bank is open and ready
+                // You can see other things you can do now below
+                if(Bank.depositAllItems()){
+                    sleep(500,1000);
+                    Bank.close();
+                };
+
+            } else {
+                // If this returns false, that means the client is still walking
+                // or interacting with the bank
+                return 2500;
+            }
+        }
+
+        MethodProvider.log(myPlayer.getAnimation());
+        return Calculations.random(220,600);
+    }
+
+
+    @Override
+    public void onExit() {
+        super.onExit();
+    }
+
+    @Override
+    public void onPaint(Graphics graphics) {
+        super.onPaint(graphics);
+    }
+}
